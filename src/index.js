ba2a62f2f988b9271368c2cb682b7e76018ddd66
yus <1069227+yus@users.noreply.github.com>
2025-08-15 02:06:05 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
+import * as color from './color';
+import * as pdf from './pdf';
+import * as story from './story';
 
+export default {
+  ...color,
+  ...pdf,
+  ...story
+};
+
+// For Node.js compatibility
+if (typeof module !== 'undefined' && module.exports) {
+  module.exports = {
+    ...color,
+    ...pdf,
+    ...story
+  };
+}
