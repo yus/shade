37afeb55dfcc1e882cf5652e751a66b6ac5ac632
yus <1069227+yus@users.noreply.github.com>
2025-08-15 01:58:08 +0800

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,1 +1,28 @@
+const path = require('path');
 
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'shade.umd.js',
+    library: {
+      name: 'Shade',
+      type: 'umd'
+    },
+    globalObject: 'this'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  }
+};
